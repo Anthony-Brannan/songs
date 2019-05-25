dbd070374be2be14b520faab8ae29bc36107a3b9
Anthony Brannan <anthonybrannan4@gmail.com>
2019-05-25 14:23:34 +0100

    Updates

---
+++ precache-manifest.fc63f666d4df545fe9bc43ca228d3587.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e1a3513d6946c67aa70ccd731302ba",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "ca03d27ad241a21f33a3",
+    "url": "/songs/static/js/2.b9c01a8d.chunk.js"
+  },
+  {
+    "revision": "5037fd33d0157a1197c7",
+    "url": "/songs/static/js/main.4620cda7.chunk.js"
+  },
+  {
+    "revision": "650915504906891d68a0",
+    "url": "/songs/static/js/runtime~main.198a175a.js"
+  }
+]);
